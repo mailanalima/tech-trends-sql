f47faf1fd1dc390a752883608efa4709499ce198
Mailana <103777331+mailanalima@users.noreply.github.com>
2026-03-18 14:12:31 +0000

    Add files via upload

--- sql_queries/consulta_tags.sql
+++ sql_queries/consulta_tags.sql
@@ -1,1 +1,10 @@
-
+SELECT 
+    tag_name, 
+    count AS numero_de_perguntas
+FROM 
+    `bigquery-public-data.stackoverflow.tags`
+WHERE 
+    count > 1000 
+ORDER BY 
+    numero_de_perguntas DESC
+LIMIT 10;
